847091c85f8efb801bbebaa4c93398af23900c50
HenryWu1113 <101792940+HenryWu1113@users.noreply.github.com>
2022-08-23 08:31:02 +0000

    Deploying to gh-pages from @ HenryWu1113/myshop-front-vuetify@8146399e6b5b640c22677b678ff916c77c68b0ad 🚀

--- assets/WhyView.d0807e22.js
+++
@@ -1,1 +0,0 @@
-import{o as a,c as t,b as o,F as r,a as e,f as s}from"./index.4b7b983c.js";const c=e("div",{id:"why_view"},[e("h2",null,"why \u590F\u96EA")],-1),d={__name:"WhyView",setup(n){return(_,l)=>(a(),t(r,null,[c,o(s)],64))}};export{d as default};
